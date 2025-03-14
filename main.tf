28be6d013fd1a536c9546dee39ee48b5a46a8257
Robert Reyes <neorob22@gmail.com>
2025-03-13 22:21:06 -0500

    added k8s deployments and services

--- main.tf
+++ main.tf
@@ -16,15 +16,22 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "aks_rg" {
-  name = "aks-resource-group"
+  name = "eshop-aks-resource-group"
   location = "southcentralus"
 }
 
+resource "azurerm_container_registry" "aks_container_registry" {
+  name = "eshopContainerRegistryXYZ"
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  location = "southcentralus"
+  sku = "Basic"
+}
+
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  name = "single-node-aks"
+  name = "eshop-single-node-aks"
   location = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
-  dns_prefix = "single-node-aks"
+  dns_prefix = "eshop-single-node-aks"
 
   default_node_pool {
     name = "default"
@@ -36,12 +43,25 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     type = "SystemAssigned"
   }
 
+  role_based_access_control_enabled = true
+
   tags = {
     Environment = "Dev"
   }
 }
 
+resource "azurerm_role_assignment" "acr_pull" {
+  scope = azurerm_container_registry.aks_container_registry.id
+  role_definition_name = "ACRPull"
+  principal_id = azurerm_kubernetes_cluster.aks_cluster.identity[0].principal_id
+  depends_on = [ azurerm_kubernetes_cluster.aks_cluster ]
+}
+
 output "kube_config" {
   value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
   sensitive = true
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.aks_container_registry.login_server
 }
